6451c0d17a9d4178e2efa07259d893bf51420ab0
Max Susman <max.susman@gmail.com>
2022-02-05 00:12:29 -0800

    Added 'Change Password' functionality to user menu.

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = "home"
 urlpatterns = [
     path("", views.home_view, name="home_page"),
     path("unauthorized", views.unauthorized_view, name="unauthorized_view"),
+    path("reset_password", views.reset_password, name="reset_password"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from rubric.models import Evolution
 from stucon.models import Student
 from gradebook.models import ObjectiveScore, TraitScore
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.urls import reverse
+
 
 
 def home_view(request):
@@ -32,3 +39,19 @@ def unauthorized_view(request):
         "home/unauthorized.html",
         {},
     )
+
+def reset_password(request):
+    if request.method == "GET":
+        pw_change_form = PasswordChangeForm(user=request.user)
+        return TemplateResponse(request, "home/password_reset.html",{
+            "pw_change_form": pw_change_form,
+        })
+    if request.method == "POST":
+        pw_change_form = PasswordChangeForm(user=request.user, data=request.POST)
+        if pw_change_form.is_valid():
+            pw_change_form.save()
+            messages.add_message(request, messages.SUCCESS, "Successfully changed password.")
+        else:
+            print(pw_change_form.error_messages)
+            messages.add_message(request, messages.ERROR, "Error: Password not changed successfully.")
+        return HttpResponseRedirect(reverse("home:home_page"))
