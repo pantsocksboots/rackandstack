b57c31037a1d8d8f1914ca1ae62c8972873a2845
Max Susman <max.susman@gmail.com>
2022-03-29 04:49:10 +0000

    Updated requirements.txt to include decouple for storing SECRET_KEY more securely.

--- rackandstack/settings.py
+++ rackandstack/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +22,10 @@ APP_NAME = "rackandstack"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w!7gdezdo2#2n0=x1r17@=)o(p()dd9s6be*(gimr4!g6!%l(j"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["www.rackandstack.dev", "localhost"]
 
